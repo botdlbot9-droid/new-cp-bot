a7bee80f6f9f3c52a54a17959546c292fabb731b
botdlbot9-droid <botdlbot9@gmail.com>
2026-08-13 09:48:45 +0530

    Create global.py

---
+++ global.py
@@ -0,0 +1,15 @@
+processing_request = False
+cancel_requested = False
+
+caption = "/cc1"
+endfilename = "/d"
+thumb = "/d"
+CR = "💫 DRM Bot"
+cwtoken = ""
+cptoken = ""
+pwtoken = ""
+vidwatermark = "/d"
+raw_text2 = "720"
+quality = "720"
+res = "1280x720"
+topic = "/d"
